a286f4477c231f29b7a9bf1a7e5f853c4c4a2db8
bsbds <69835502+bsbds@users.noreply.github.com>
2023-07-14 10:56:40 +0800

    feat: add communication api for sidecar and deploy operators
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

---
+++ operator-api/README.md
@@ -0,0 +1,3 @@
+# operator-api
+
+This crate provide API types that used in operators.
---
+++ operator-api/src/lib.rs
@@ -0,0 +1,35 @@
+use serde::{Deserialize, Serialize};
+
+/// Heartbeat status
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct HeartbeatStatus {
+    /// the name of the sidecar operator
+    pub name: String,
+    /// the timestamp of this status
+    pub timestamp: u64,
+    /// reachable sidecar operator ids
+    pub reachable: Vec<String>,
+}
+
+impl PartialOrd for HeartbeatStatus {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.timestamp.cmp(&other.timestamp))
+    }
+}
+
+impl Ord for HeartbeatStatus {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.timestamp.cmp(&other.timestamp)
+    }
+}
+
+impl HeartbeatStatus {
+    /// Creates a new `HeartbeatStatus`
+    pub fn new(name: String, timestamp: u64, reachable: Vec<String>) -> Self {
+        Self {
+            name,
+            timestamp,
+            reachable,
+        }
+    }
+}
