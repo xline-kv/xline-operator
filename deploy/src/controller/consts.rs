b2fbbbbba5820e751210eb3b09f2448900635ae8
iGxnon <igxnon@gmail.com>
2023-07-16 13:01:09 +0800

    feat: add emptyDir volume for custom resource
    Add emptyDir as an extra volume when the ``.spec.data` is not provided
    
    Signed-off-by: iGxnon <igxnon@gmail.com>

--- deploy/src/controller/consts.rs
+++ deploy/src/controller/consts.rs
@@ -4,3 +4,5 @@ use std::time::Duration;
 pub(super) const DEFAULT_REQUEUE_DURATION: Duration = Duration::from_secs(600);
 /// The field manager identifier of deploy operator
 pub(super) const FIELD_MANAGER: &str = "xlineoperator.datenlord.io/deployoperator";
+/// The emptyDir volume name of each pod
+pub(crate) const DATA_EMPTY_DIR_NAME: &str = "xline-data-empty-dir";
