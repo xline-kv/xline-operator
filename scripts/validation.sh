4812e7ded4a6294b2f2fdcd049b3f82b3649446b
iGxnon <igxnon@gmail.com>
2023-07-26 15:29:41 +0800

    ci: add simple chaos validation
    Signed-off-by: iGxnon <igxnon@gmail.com>

--- scripts/validation.sh
+++ scripts/validation.sh
@@ -5,6 +5,7 @@ DNS_SUFFIX="cluster.local"
 NAMESPACE="default"
 XLINE_PORT="2379"
 ENDPOINTS=""
+LOG_SYNC_TIMEOUT=30
 
 # start a tester pod to run etcdctl
 kubectl delete pod/tester 2>/dev/null
@@ -16,9 +17,9 @@ kubectl debug tester --image=gcr.io/etcd-development/etcd:v3.5.5 --container=etc
 sleep 5
 kubectl wait --for=condition=containersready pod/tester --timeout=10m
 
-# avoid mysterious "Error from server: error dialing backend: EOF" error
 etcdctl() {
-  for ((i = 0; i < 5; i++)); do
+  # avoid mysterious "Error from server: error dialing backend: EOF" error
+  for ((i = 0; i < 15; i++)); do
     if output=$(kubectl exec tester -c etcdctl -- etcdctl --endpoints="$ENDPOINTS" $@ 2>&1); then
       echo -e "$output"
       return
@@ -48,6 +49,7 @@ scale_cluster() {
     echo "got size: $got"
     exit 1
   fi
+  mk_endpoints "$1"
 }
 
 run_expect() {
@@ -69,19 +71,65 @@ basic_validation() {
   mk_endpoints 3
   run_expect "put A 1" "OK"
   run_expect "get A" "A\n1"
+  mk_endpoints 1
+  run_expect "put A 1" "OK"
+  run_expect "get A" "A\n1"
 }
 
 scale_validation() {
   echo "=== Scale Validation ==="
   scale_cluster 5
-  mk_endpoints 5
-  run_expect "put A 1" "OK"
-  run_expect "get A" "A\n1"
-  scale_cluster 3
-  mk_endpoints 3
   run_expect "put A 1" "OK"
   run_expect "get A" "A\n1"
 }
 
+random() {
+  echo $((RANDOM % $1))
+}
+
+monkeys() {
+  size=$1
+  iters=$2
+  max_kill=$((size / 2))
+  killed=false
+  echo "monkeys: size=$size, iters=$iters, max_kill=$max_kill"
+  for ((i = 0; i < iters; i++)); do
+    case $(random 3) in
+    0)
+      echo "monkeys: put get"
+      value=$(random 100)
+      run_expect "put A $value" "OK"
+      run_expect "get A" "A\n$value"
+      ;;
+    1)
+      echo "monkeys: drop pods"
+      if [ "$killed" = true ]; then
+        kubectl wait --for=jsonpath='{.status.readyReplicas}'="$size" sts/$CLUSTER_NAME --timeout=10m
+        echo "wait for log sync" && sleep $LOG_SYNC_TIMEOUT
+      fi
+      killed=true
+      ready=$(kubectl get sts/$CLUSTER_NAME -o=jsonpath='{.status.readyReplicas}')
+      to_kill=$((ready + max_kill - size))
+      for ((y = 0; y < to_kill; y++)); do
+        name=$CLUSTER_NAME-$(random "$size")
+        kubectl delete pod/"$name" --force --grace-period=0 2>/dev/null
+      done
+      ;;
+    2)
+      echo "monkeys: sleep"
+      ;;
+    esac
+  done
+  kubectl wait --for=jsonpath='{.status.readyReplicas}'="$size" sts/$CLUSTER_NAME --timeout=10m
+  echo "wait for log sync" && sleep $LOG_SYNC_TIMEOUT
+}
+
+chaos_validation() {
+  echo "=== Chaos Validation ==="
+  scale_cluster 5
+  monkeys 5 20
+}
+
 basic_validation
 scale_validation
+chaos_validation
