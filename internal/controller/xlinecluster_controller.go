f75feae9390e4cafcc03a7145cc6e3749b745112
Phoeniix Zhao <Phoenix500526@163.com>
2023-11-24 09:44:41 +0800

    feat(deploy): add reconciler and sync for XlineCluster
    Signed-off-by: Phoeniix Zhao <Phoenix500526@163.com>

--- internal/controller/xlinecluster_controller.go
+++ internal/controller/xlinecluster_controller.go
@@ -18,13 +18,14 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
+	xapi "github.com/xline-kv/xline-operator/api/v1alpha1"
+	"github.com/xline-kv/xline-operator/internal/reconciler"
+	"github.com/xline-kv/xline-operator/internal/util"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	xlinekvstoredatenlordcomv1alpha1 "github.com/xline-kv/xline-operator/api/v1alpha1"
 )
 
 // XlineClusterReconciler reconciles a XlineCluster object
@@ -36,27 +37,66 @@ type XlineClusterReconciler struct {
 //+kubebuilder:rbac:groups=xline.kvstore.datenlord.com,resources=xlineclusters,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=xline.kvstore.datenlord.com,resources=xlineclusters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=xline.kvstore.datenlord.com,resources=xlineclusters/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the XlineCluster object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
 func (r *XlineClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	recCtx := reconciler.NewReconcileContext(r.Client, r.Scheme, ctx)
+	// obtain CR
+	cr := &xapi.XlineCluster{}
+	exist, err := recCtx.Exist(req.NamespacedName, cr)
+	if err != nil {
+		return ctrl.Result{Requeue: true}, err
+	}
+	// skip reconciling process when it has been deleted
+	if !exist {
+		recCtx.Log.Info(fmt.Sprintf("XlineCluster(%s) has been deleted", util.K8sObjKeyStr(req.NamespacedName)))
+		return ctrl.Result{}, nil
+	}
+	rec := reconciler.XlineClusterReconciler{ReconcileContext: recCtx, CR: cr}
 
-	// TODO(user): your logic here
+	curSpecHash := util.Md5HashOr(cr.Spec, "")
+	isFirstCreated := cr.Status.LastApplySpecHash == nil
+	specHasChanged := isFirstCreated || *cr.Status.LastApplySpecHash != curSpecHash
+	preRecCompleted := cr.Status.Stage == xapi.StageComplete
 
-	return ctrl.Result{}, nil
+	if isFirstCreated && cr.Status.Stage == "" {
+		recCtx.Log.Info(fmt.Sprintf("XlineCluster(%s) is created for the first time", util.K8sObjKeyStr(req.NamespacedName)))
+	}
+	if specHasChanged {
+		recCtx.Log.Info(fmt.Sprintf("XlineCluster(%s) spec has been updated", util.K8sObjKeyStr(req.NamespacedName)))
+	}
+
+	// reconcile the sub resource of XlineCluster
+	var recErr error
+	if specHasChanged || !preRecCompleted {
+		recRs := rec.Reconcile()
+		recErr = recRs.Err
+		cr.Status.XlineClusterRecStatus = recRs.AsXlineClusterRecStatus()
+		// when reconcile process competed success, update the last apply spec hash
+		if recRs.Stage == xapi.StageComplete {
+			cr.Status.LastApplySpecHash = &curSpecHash
+		}
+	}
+	// sync the status of CR
+	syncRs, syncErr := rec.Sync()
+	cr.Status.XlineClusterSyncStatus = syncRs
+	// update status
+	updateErr := r.Status().Update(ctx, cr)
+
+	// merge error at different reconcile phases
+	errSet := StCtrlErrSet{
+		Rec:    recErr,
+		Sync:   syncErr,
+		Update: updateErr,
+	}
+	return errSet.AsResult()
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *XlineClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&xlinekvstoredatenlordcomv1alpha1.XlineCluster{}).
+		For(&xapi.XlineCluster{}).
 		Complete(r)
 }
